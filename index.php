64c9b8905bd3215b187c1d589ee331a6790ec003
paper <cyclingpaper@gmail.com>
2017-01-24 19:54:05 -0500

    added text to see difference between systems

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
 	<body>
 		<a>Hey there.  I'm an example because the tutorial files are corrupt.  Good work there buddy, perhaps you should fix them.</a>
 		<a>I'm just an update to the index file.</a>
+		<a>Another update for cross system example.</a>
 </html>
